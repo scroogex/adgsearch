cf45dd2df40fc50e8f5802a5a6b87c72422f85e6
scroogex <111561404+scroogex@users.noreply.github.com>
2022-08-21 16:11:05 +0530

    Update pm_filter.py

--- NL_BOTZ/pm_filter.py
+++ NL_BOTZ/pm_filter.py
@@ -1591,7 +1591,7 @@ async def advantage_spell_chok(msg):
             InlineKeyboardButton('🔍 ɢᴏᴏɢʟᴇ 🔎', url=f'https://google.com/search?q='),
             InlineKeyboardButton(' 🔍 ʏᴀɴᴅᴇx 🔎', url=f'https://yandex.com/search?text=')
         ],[
-            InlineKeyboardButton("🇮🇳 ᴛʀᴀɴsʟᴀᴛᴇ ᴛᴏ ᴍᴀʟᴀʏᴀʟᴀᴍ 🇮🇳", callback_data="try")
+            InlineKeyboardButton("Our Group", url"https://t.me/asiandramagroup1")
         ]]        
         k=await msg.reply_photo(photo="https://telegra.ph/file/4bb1968bd091453b0070c.jpg", caption=script.ENGLISHSPELL_TXT, reply_markup=InlineKeyboardMarkup(btn))    
         await asyncio.sleep(40)
